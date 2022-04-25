e6cf89a4c73096683cba118a2eac263951c979d5
Alex Waller <alex@artofalexwaller.com>
2022-04-25 14:14:46 -0400

    sortEmployees

--- NewReactApp-5/src/components/EmployeeDiv.jsx
+++ NewReactApp-5/src/components/EmployeeDiv.jsx
@@ -3,11 +3,11 @@ import Header from './Header';
 import SortButton from './SortButton';
 import EmployeeList from './EmployeeList';
 
- const EmployeeDiv = ({ employees }) => {
+ const EmployeeDiv = ({ employees, sortEmployees }) => {
   return (
     <div>
         <Header  text="Employee Directory"/>
-        <SortButton text="Sort Employees"/>
+        <SortButton text="Sort Employees" sortEmployees={sortEmployees}/>
         <EmployeeList employees={employees}/>
     </div>
   )
--- NewReactApp-5/src/components/SortButton.jsx
+++ NewReactApp-5/src/components/SortButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
- const SortButton = ({text}) => {
+ const SortButton = ({text, sortEmployees}) => {
   return (
-    <button className='btn btn-primary'>{text}</button>
+    <button className='btn btn-primary' onClick={sortEmployees}>{text}</button>
   )
 }
 
